87a21ddbb7b7de03e786409c32a314f559301b80
Pankaj Kumar <pankajkumardas727@gmail.com>
2021-11-13 15:45:27 +0530

    UIdone

--- _site/assets/main.js
+++ _site/assets/main.js
@@ -1,3 +1,12 @@
+require(["/library_path/zip-fs.min.js"], zip => {
+    const { fs, configure } = zip;
+    // ...
+});
+
+zip.configure({
+    useWebWorkers: false
+});
+
 var dropArea = document.querySelector(".container");
 
 dropArea.addEventListener("dragover", (event) => {
@@ -10,6 +19,52 @@ dropArea.addEventListener("dragleave", (event) => {
     dropArea.classList.remove("active");
 })
 
-dropArea.addEventListener("drop", (event) => {
-    event.preventDefault();
-})
+// dropArea.addEventListener("drop", (event) => {
+//     event.preventDefault();
+// })
+
+dropArea.addEventListener("drop", async event => {
+    const target = getFileElement(event.target);
+    stopEvent(event);
+    if (target) {
+        const targetNode = getFileNode(target);
+        if (event.dataTransfer && event.dataTransfer.files && event.dataTransfer.files.length) {
+            const item = event.dataTransfer.items[0];
+            const file = event.dataTransfer.files[0];
+            if (item && file && !item.type.includes("zip") && !file.name.endsWith(".zip") && item.webkitGetAsEntry !== undefined) {
+                const fileEntry = await item.webkitGetAsEntry();
+                const entry = await model.addFileSystemEntry(fileEntry, targetNode);
+                if (fileEntry.isDirectory) {
+                    selectDirectory(target);
+                    expandTree(targetNode);
+                }
+                refreshTree();
+                refreshListing();
+                if (fileEntry.isDirectory) {
+                    selectDirectory(findFileElement(entry.id));
+                }
+            } else {
+                const file = item.getAsFile();
+                try {
+                    await model.importZip(file, targetNode);
+                } catch (error) {
+                    alert(error);
+                }
+                selectDirectory(target);
+                expandTree(targetNode);
+                refreshTree();
+                refreshListing();
+            }
+        } else {
+            const srcNode = getFileNode(selectedDrag);
+            if (targetNode != srcNode && targetNode != srcNode.parent && !targetNode.isDescendantOf(srcNode)) {
+                model.move(srcNode, targetNode);
+                targetNode.expanded = target.open = true;
+                refreshTree();
+                refreshListing();
+            } else {
+                hoveredElement.classList.remove("drag-over");
+            }
+        }
+    }
+}, false); 
